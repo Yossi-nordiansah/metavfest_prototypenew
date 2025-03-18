0390db9f79e4feec4dee3682372267dd73a9ac45
Yosi <yosirifaiputra@gmail.com>
2025-03-18 20:29:57 +0700

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        flicker: {
+          "0%, 100%": { filter: "drop-shadow(0 0 5px #0096CF)" },
+          "50%": { filter: "drop-shadow(0 0 1px #0096CF)" },
+        },
+        float: {
+          "0%, 100%": { transform: "translateY(0px)" },
+          "50%": { transform: "translateY(-10px)" },
+        },
+      },
+      animation: {
+        flicker: "flicker 2s infinite ease-in-out",
+        float: "float 3s ease-in-out infinite", // ✅ Pastikan ini ada!
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#0096CF",
+        secondary: "#201658",
+        tosca: "#20F1EE",
+        segaSunset: "var(--sega-sunset)",
+        azureRadiance: "var(--azure-radiance)",
+        frostedWhisper: "var(--frosted-whisper)",
+      },
+      fontFamily: {
+        japanese: ["go3v2"],
+        radjdhani_semibold: ["radjdhani_semibold"],
+        radjdhani_medium: ["radjdhani_medium"],
+        radjdhani_bold: ["radjdhani_bold"],
+        radjdhani_regular: ["radjdhani_regular"],
+        radjdhani_light: ["radjdhani_light"],
+      },
+      scrollBehavior: ["responsive"], // ✅ Pindahkan ke sini
+    },
+  },
+  plugins: [],
+};
+
+export default config;
