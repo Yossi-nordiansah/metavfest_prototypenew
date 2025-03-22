3d85e02d7c9eae48fd33800c0b27df51459f8c75
Yosi <yosirifaiputra@gmail.com>
2025-03-22 11:04:41 +0700

    Kurang War Tiket dan Bantu perbaiki tailwindnya biar responsive

--- app/_components/LayoutWrapper.jsx
+++ app/_components/LayoutWrapper.jsx
@@ -4,12 +4,20 @@ import { usePathname } from "next/navigation";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import Navtop from "./navtop";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function LayoutWrapper({ children }) {
   const pathname = usePathname();
   const isAdminPage = pathname.startsWith("/admin");
   const isLoginPage = pathname.startsWith("/auth/login");
   const shouldHideLayout = isAdminPage || isLoginPage;
+
+  {/** AOS ANIMATIONS*/}
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
   return (
     <>
--- app/_components/hero.jsx
+++ app/_components/hero.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { FaLocationDot } from "react-icons/fa6";
 
 const Hero = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center text-white lg:h-screen py-10 overflow-visible pt-10 pb-20 lg:pb-32">
+    <div className="relative flex flex-col items-center justify-center text-white lg:h-screen py-10 overflow-hidden pt-10 pb-20 lg:pb-32 bg-hero-gradient">
       {/* Background Image */}
       <img
         src="/images/hero-1.png"
@@ -18,28 +17,6 @@ const Hero = () => {
           alt="floating title"
           className="w-[95vw] lg:w-[85vw] md:w-[75vw] sm:w-[65vw] max-w-none h-auto animate-float mt-auto lg:mt-32"
         />
-
-        {/* Title & Description */}
-        <p className="px-4 text-2xl sm:text-4xl font-bold bg-black bg-clip-text text-transparent">
-          MetaVFest - The Future of Web3 &
-        </p>
-        <p className="px-4 text-2xl sm:text-4xl font-bold bg-black bg-clip-text text-transparent">
-          Blockchain in Surabaya
-        </p>
-        <p className="px-4 sm:text-xl mt-2 font-bold bg-black bg-clip-text text-transparent">
-          "Explore. Learn. Network. Experience the World of Crypto, Metaverse, and Innovation."
-        </p>
-        <p className="px-4 sm:text-xl mt-6 font-bold bg-black bg-clip-text text-transparent">
-          <a
-            href="https://www.google.com/maps/place/Balai+Pemuda+Surabaya/@-7.2639727,112.7427289,17z/data=!3m1!4b1!4m6!3m5!1s0x2dd7f9617fe40b67:0x987ff0249bb2e08a!8m2!3d-7.263978!4d112.7453038!16s%2Fg%2F1pzr1w7sd?entry=ttu&g_ep=EgoyMDI1MDMxMi4wIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-black"
-          >
-            Balai Pemuda Surabaya
-          </a> 
-          <FaLocationDot className="inline text-red-700" /> | 11 Mei 2025
-        </p>
       </div>
     </div>
   );
